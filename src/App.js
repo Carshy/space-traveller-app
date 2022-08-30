a63303545fc3514c22d34d7a69f5f37613cb3b5b
Collins <mussocollins@gmail.com>
2022-08-30 18:44:00 +0300

    Add the navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to React Collins and Kidist</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route element={<Rockets />} path="/" />
+          <Route element={<Missions />} path="/missions" />
+          <Route element={<Profile />} path="/profiles" />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navigation.css';
+import companyLogo from '../images/companyLogo.PNG';
 
 function Navigation() {
   return (
     <div>
       <nav className="navigation-section">
-        <a href="/">
-          <img src="/" alt="space travellers" />
-          Space Travellers Hub
+        <a className="nav-link" href="/">
+          <img src={companyLogo} alt="travellers" />
         </a>
         <ul className="nav-group">
-          <li className="nav-item">Rockets</li>
-          <li className="nav-item">Missions</li>
-          <li className="nav-item">My Profile</li>
+          <li>
+            <Link to="/" className="nav-item">
+              Rockets
+            </Link>
+          </li>
+          <li>
+            <Link to="/missions" className="nav-item">
+              Missions
+            </Link>
+          </li>
+          <li>
+            <Link to="/profiles" className="nav-item">
+              My Profile
+            </Link>
+          </li>
         </ul>
       </nav>
     </div>
