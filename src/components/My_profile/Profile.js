45fdf940e9984a273d95a806fdce13c4400bf124
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-09-01 20:44:12 +0300

    Update files to change tag and display mission in myProfile section

--- src/components/My_profile/Profile.js
+++ src/components/My_profile/Profile.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './profile.css';
+import { PopulateMissionProfile } from '../../redux/mission/mission';
 
-function Profile() {
+const Profile = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missionReducer);
+
+  useEffect(() => {
+    dispatch(PopulateMissionProfile());
+  }, []);
+
   return (
-    <div>
-      <h1>Our Profile</h1>
+    <div className="myProfile">
+      <section className="my-comp">
+        <h2>My Mission</h2>
+        <ul className="reserved-container">
+          { missions.map((mission) => (mission.reserved === true ? (
+            <li className="reserved-item" key={mission.mission_id}>
+              {' '}
+              {mission.mission_name}
+              {' '}
+            </li>
+          ) : null))}
+        </ul>
+      </section>
     </div>
   );
-}
+};
 
 export default Profile;
