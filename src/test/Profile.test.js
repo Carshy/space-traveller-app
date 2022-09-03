20a4fe6ac6a320708735fd5c650beac3db92142d
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-09-03 11:27:43 +0300

    Fix linter errors

--- src/test/Profile.test.js
+++ src/test/Profile.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../redux/configureStore';
+import store from '../redux/ConfigureStore';
 import Profile from '../components/My_profile/Profile';
 
 test('renders correctly', () => {
