2d44b3d67c2069f2d04ef329ede7dc16380f2f47
Collins <mussocollins@gmail.com>
2023-03-07 00:52:00 +0300

    Add footer to mission

--- src/components/Mission/MissionList.js
+++ src/components/Mission/MissionList.js
@@ -1,12 +1,16 @@
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-confusing-arrow */
 import React, { useState, useEffect } from 'react';
+// import { RxDoubleArrowDown } from 'react-icons/rx';
+import { FaFacebookF, FaTwitter } from 'react-icons/fa';
+import { AiOutlineGooglePlus } from 'react-icons/ai';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import CountUp from 'react-countup';
 import ScrollTrigger from 'react-scroll-trigger';
 import { LoadMissions } from '../../redux/mission/mission';
 import MissionPage from './Mission';
 import './mission.scss';
+import '../Home/Home.scss';
 
 const Missions = () => {
   const [counterOn, setCounterOn] = useState(false);
@@ -77,6 +81,71 @@ const Missions = () => {
           />
         ))}
       </ul>
+
+      <div className="app__footer">
+        <div className="app__footer-upper">
+          <div className="app__footer-destinations">
+            <div className="moon">
+              <p>Moon</p>
+            </div>
+            <div className="mars">
+              <p>Mars</p>
+            </div>
+          </div>
+          <div className="app__footer-aboutterms">
+            <h5>About Us</h5>
+            <h5>FAQ</h5>
+            <h5>Privacy & Terms</h5>
+          </div>
+          <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+            <div
+              className="app__counterup-home"
+            >
+              <div>
+                <h1>
+                  {counterOn && <CountUp start={0} end={321} duration={2} delay={0} />}
+                  +
+                </h1>
+                <p>LAUNCHES</p>
+              </div>
+
+              <div>
+                <h1>
+                  {counterOn && <CountUp start={0} end={205} duration={2} delay={0} />}
+                  +
+                </h1>
+                <p>LANDINGS</p>
+              </div>
+
+              <div>
+                <h1>
+                  {counterOn && <CountUp start={0} end={187} duration={2} delay={0} />}
+                  +
+                </h1>
+                <p>REFLIGHTS</p>
+              </div>
+            </div>
+          </ScrollTrigger>
+        </div>
+        <hr />
+        <div className="app__footer-lower">
+          <p className="footer-lower-header">
+            S.Connect
+          </p>
+          <div>
+            <a href="https://www.facebook.com/collins.mussoko" className="footer-socials">
+              <FaFacebookF />
+            </a>
+            <a href="https://www.facebook.com/collins.mussoko" className="footer-socials">
+              <AiOutlineGooglePlus />
+            </a>
+            <a href="https://twitter.com/CarshyCollins" className="footer-socials">
+              <FaTwitter />
+            </a>
+          </div>
+          <p className="footer__copyright">App Created by ©Collins Musoko</p>
+        </div>
+      </div>
     </section>
   );
 };
